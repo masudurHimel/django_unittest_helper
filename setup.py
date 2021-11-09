60f847ff5c760a22b7a0dfd943cfb99099cc5f0a
masudurHimel <masudurhimel@gmail.com>
2021-11-09 17:46:27 +0600

    setup updated

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     author='Md. Masudur Rahman',  # Type in your name
     author_email='masudurhimel@gmail.com',  # Type in your E-Mail
     url='https://github.com/masudurHimel/django_unittest_helper',  # Provide either the link to your github or to your website
-    download_url='https://github.com/masudurHimel/TestLibrary_MR/archive/refs/tags/v_02.tar.gz',  # I explain this later on
+    download_url='https://github.com/masudurHimel/django_unittest_helper/archive/refs/tags/v_01.tar.gz',  # I explain this later on
     keywords=['django', 'unittest'],  # Keywords that define your package best
     install_requires=['Pillow'],
     classifiers=[
