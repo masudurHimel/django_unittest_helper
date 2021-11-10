f810dccd29a957d8aaad78c577958efdc4c92a78
masudurHimel <masudurhimel@gmail.com>
2021-11-10 17:01:44 +0600

    updated

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
-from distutils.core import setup
+from setuptools import setup
 
-with open("../README.md", "r", encoding="utf-8") as fh:
+with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setup(
     name='django_unittest_helper',  # How you named your package folder (MyLib)
     packages=['django_unittest_helper'],  # Chose the same as "name"
-    version='0.2',  # Start with a small number and increase it with every change you make
+    version='0.3',  # Start with a small number and increase it with every change you make
     license='MIT',  # Chose a license from here: https://help.github.com/articles/licensing-a-repository
     description='Helper package for django unittest',  # Give a short description about your library
     long_description=long_description,
@@ -14,7 +14,7 @@ setup(
     author='Md. Masudur Rahman',  # Type in your name
     author_email='masudurhimel@gmail.com',  # Type in your E-Mail
     url='https://github.com/masudurHimel/django_unittest_helper',  # Provide either the link to your github or to your website
-    download_url='https://github.com/masudurHimel/django_unittest_helper/archive/refs/tags/v_02.tar.gz',  # I explain this later on
+    download_url='https://github.com/masudurHimel/django_unittest_helper/archive/refs/tags/v_03.tar.gz',  # I explain this later on
     keywords=['django', 'unittest'],  # Keywords that define your package best
     install_requires=['Pillow'],
     classifiers=[
